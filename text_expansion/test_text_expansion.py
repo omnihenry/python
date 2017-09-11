0e451c421ff14f5a420bb8a471a723449269f6f2
Henry <omniwhb@yahoo.com>
2017-09-11 10:04:18 -0400

    comments on unittest

--- text_expansion/test_text_expansion.py
+++ text_expansion/test_text_expansion.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python
+# title           :test_text_expansion.py
+# description     :This is the unittest script for text_expansion
+# author          :Hongbo Wang
+# date            :20170820
+# version         :0.1
+# usage           :python3 -m unittest test_text_expansion.py
+#                  python3 test_text_expansion.py
+# notes           :
+# python_version  : > 3 
+#==============================================================================
+
 import unittest
 from text_expansion import *
 
